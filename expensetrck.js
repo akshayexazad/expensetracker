6f5822dd476e499337e9e86dd5f615622103b477
Akshay Kumar <akshayexazad@gmail.com>
2022-10-21 14:24:05 +0530

    2nd commit

--- expensetrck.js
+++ expensetrck.js
@@ -3,7 +3,7 @@ function saveToLocalStorage(event){
     let enterAmount=event.target.expamount.value;
     let enterDescription=event.target.entdescription.value;
     let catagorySelect=event.target.selectCatagory.value;
-    console.log(catagorySelect)
+    // console.log(catagorySelect)
     let obj={
        Amount: enterAmount,
         Description:enterDescription,
@@ -17,16 +17,15 @@ function DisplayOnUi(items){
   let   ParentLi=document.getElementById('listOfUsers');
   let   ChildsOfLi=`<li id=${items.Description}>Amount= ${items.Amount}  Description=${items.Description} catagory= ${items.Catagory}
                 <button onclick=deleteItem('${items.Description}')>DeleteItem</button>
-                <button onclick=editItem('${items.Amount},${items.Description},${items.Catagory}')>editItem</button>
+                <button onclick=editItem('${items.Amount}','${items.Description}','${items.Catagory}')>editItem</button>
                 </li>`;
      ParentLi.innerHTML= ParentLi.innerHTML+ChildsOfLi;   
 
-    console.log(ChildsOfLi)
 
 }
 // delete from local storage;
 function deleteItem(Description){
-    localStorage.removeItem(Description);
+    localStorage.removeItem(Description); 
     removeItemFromUi(Description);
 }
 //delete fron ui;
@@ -36,8 +35,31 @@ function removeItemFromUi(Description){
     ParentLi.removeChild(delteChild);
 }
 function editItem(amount,description,catagory){
- document.getElementById('expamount').value=amount;
- document.getElementById('entdescription').value=description;
- document.getElementById('selectCatagory').value=catagory;
- deleteItem(description);
-}
+   document.getElementById('expamount').value= amount;
+   document.getElementById('entdescription').value=description;
+   document.getElementById('selectCatagory').value=catagory;
+   removeItemFromUi(description);
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function editItem(amount,description,catagory){
+//  document.getElementById('expamount').value=amount;
+//  document.getElementById('entdescription').value=description;
+//  document.getElementById('selectCatagory').value=catagory;
+//  deleteItem(description);
+// }
